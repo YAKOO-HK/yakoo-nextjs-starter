77a9b1a02e90850f8db83b5c329f1adfb94b6804
arku3 <hmku@yakoo.com.hk>
2024-02-22 16:30:55 +0800

    Refactor FileDropzoneField component

--- src/components/form/FileDropzoneField.tsx
+++ src/components/form/FileDropzoneField.tsx
@@ -13,57 +13,53 @@ export interface FileDropzoneFieldProps extends Omit<React.HTMLAttributes<HTMLDi
   name: string;
 }
 
-export const FileDropzoneField = ({
-  id,
-  name,
-  value = [],
-  onChange,
-  dropzoneProps = {},
-  isError,
-  className,
-  ...props
-}: FileDropzoneFieldProps) => {
-  const genId = React.useId();
-  const { getRootProps, getInputProps, open } = useDropzone({
-    // Disable click and keydown behavior
-    noClick: true,
-    noKeyboard: true,
-    ...dropzoneProps,
-    onDropAccepted: onChange,
-  });
+export const FileDropzoneField = React.forwardRef<React.ElementRef<'input'>, FileDropzoneFieldProps>(
+  function FileDropzoneField(
+    { id, name, value = [], onChange, dropzoneProps = {}, isError, className, ...props },
+    ref
+  ) {
+    const genId = React.useId();
+    const { getRootProps, getInputProps, open } = useDropzone({
+      // Disable click and keydown behavior
+      noClick: true,
+      noKeyboard: true,
+      ...dropzoneProps,
+      onDropAccepted: onChange,
+    });
 
-  return (
-    <div
-      {...getRootProps()}
-      {...props}
-      className={cn('flex flex-col items-center justify-center rounded-md border border-border p-4', {
-        'border-destructive': isError,
-      })}
-    >
-      <input {...getInputProps()} name={name} id={id || genId} />
-      {value?.length > 0 ? (
-        value?.map((file, index) => (
-          <div key={`${index}-${file.name}`} className="space-between mb-2 flex w-full flex-row items-end gap-2">
-            <p>{file.name}</p>
-            <p className="text-sm">({filesize(file.size) as string})</p>
-          </div>
-        ))
-      ) : (
-        <>
-          <UploadIcon />
-          <p className="mb-2">Select a file or drag here</p>
-        </>
-      )}
-      <div className="space-1 flex flex-row">
-        <Button onClick={open} type="button">
-          Browse
-        </Button>
-        {value?.[0] ? (
-          <Button onClick={() => onChange([])} variant="ghost" type="button">
-            Clear
+    return (
+      <div
+        {...getRootProps()}
+        {...props}
+        className={cn('flex flex-col items-center justify-center rounded-md border border-border p-4', {
+          'border-destructive': isError,
+        })}
+      >
+        <input ref={ref} {...getInputProps()} name={name} id={id || genId} />
+        {value?.length > 0 ? (
+          value?.map((file, index) => (
+            <div key={`${index}-${file.name}`} className="space-between mb-2 flex w-full flex-row items-end gap-2">
+              <p>{file.name}</p>
+              <p className="text-sm">({filesize(file.size) as string})</p>
+            </div>
+          ))
+        ) : (
+          <>
+            <UploadIcon />
+            <p className="mb-2">Select a file or drag here</p>
+          </>
+        )}
+        <div className="space-1 flex flex-row">
+          <Button onClick={open} type="button">
+            Browse
           </Button>
-        ) : null}
+          {value?.[0] ? (
+            <Button onClick={() => onChange([])} variant="ghost" type="button">
+              Clear
+            </Button>
+          ) : null}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
